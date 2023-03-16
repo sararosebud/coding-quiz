0f1a8ef41422d25d1b758c8e0122c1dff9261430
Sara Wendland <info@rosebudsara.com>
2023-03-16 16:50:57 -0500

    updated javascript to create a dynamic app

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,127 @@
 // declare the values of each part of the app
-var startButton = document.getElementById("#startButton");
-var timer = document.getElementById("#timer");
-var scoreProgress = document.getElementById("#scoreProgress");
-var quiz = document.getElementById("#questionbox");
-var questions = document.getElementById("questions");
+var startButton = document.getElementById("startButton");
+var timer = document.getElementById("timer");
+var scoreProgress = document.getElementById("scoreProgress");
+var quiz = document.getElementById("questionbox");
+var questionsEl = document.getElementById("questions");
 var reset = document.getElementById("reset")
 var highScoreTracker = document.getElementById("highScoreTracker");
+var questionContainer = document.getElementById("question-container");
+var questionIndex = 0;
+var highScoreContainer = document.getElementById("high-score-container");
+
+var questions = [
+  {
+    question:"Question1", 
+    choices: ["A", "b", "c", "d"], 
+    answer: "d"
+  }, 
+  {
+    question:"Question2", 
+    choices: ["A", "b", "c", "d"], 
+    answer: "d"
+  },
+  {
+    question:"Question3", 
+    choices: ["A", "b", "c", "d"], 
+    answer: "d"
+  },
+  {
+    question:"Question4", 
+    choices: ["A", "b", "c", "d"], 
+    answer: "d"
+  },
+  {
+    question:"Question5", 
+    choices: ["A", "b", "c", "d"], 
+    answer: "d"
+  },
+] 
+
+function renderQuestions() {
+  var quizBox = document.createElement('div');
+  var prompt =document.createElement('p');
+  prompt.textContent = questions[questionIndex].question
+  quizBox.append(prompt)
+
+  for (i=0; i < questions[questionIndex].choices.length; i++) {
+    var choice = document.createElement('button');
+    choice.textContent = questions[questionIndex].choices[i]
+    choice.style.display = "block"
+    quizBox.append(choice)
+  }
+  
 
 
+  questionContainer.append(quizBox)
 
 
+}
 
-// const startButton = document.getElementById("startButton");
-//   startButton.addEventListener("click", function(e) {
-//     console.log('clickedbutton')
-
-//   });
-
-
-
-
-
-// timer function
-var timeLeft = 5;
-var quizTimer = setInterval(function () {
-  if (timeLeft <= 0) {
-    clearInterval(quizTimer);
-    document.getElementById("timer").innerHTML = "Game Over!";
-    // document.getElementById("startbutton").style.display = "none";
-    document.getElementById("questionbox").style.display = "none";
-    document.getElementById("highScoreTracker").style.display = "block";
-    
+function handleQuestionClick(e) {
+  var questionClicked = e.target.textContent
+  if (questionClicked === questions[questionIndex].answer) {
+    console.log('correct')
 
   } else {
-    document.getElementById("timer").innerHTML =
-      timeLeft + " seconds remaining ";
+    console.log('incorrect')
   }
-  timeLeft -= 1;
 
-}, 1000);
+  questionIndex++
+
+  questionContainer.innerHTML = ""
+
+  if (questionIndex > questions.length - 1) {
+    endQuiz()
+  } else {
+    renderQuestions()   
+
+  }
+
+
+  }
+
+function endQuiz() {
+  questionContainer.style.display = "none";
+  highScoreContainer.classList.remove('hide');
+
+}
+
+
+startButton.addEventListener("click", startQuiz)
+
+questionContainer.addEventListener('click', handleQuestionClick)
+
+function startQuiz() {
+
+  // timer function
+  var timeLeft = 5;
+  var quizTimer = setInterval(function () {
+    if (timeLeft <= 0) {
+      clearInterval(quizTimer);
+      document.getElementById("timer").innerHTML = "Game Over!";
+     
+      
+      
+      
+    } else {
+      document.getElementById("timer").innerHTML =
+      timeLeft + " seconds remaining ";
+    }
+    timeLeft -= 1;
+    
+  }, 1000);
+
+    renderQuestions();
+
+}
+
+
+
+
+
+
+
 
 
 
@@ -51,4 +134,29 @@ var quizTimer = setInterval(function () {
 // const initials = prompt("Enter Your Initials");
 // document.getElementById("highScoreTracker").innerHTML = "High Score, " + initials;
 
+// create questions
+// questions = [
+//   new Question(
+//     "this is question one?",
+//     ["answers", "answers", "Correct Answer", "answer"],
+//     "Correct Answer"
+//   ),
+//   new Question(
+//     "Question two?",
+//     ["answers", "Correct Answer", "answer", "answer"],
+//     "Correct Answer"
+//   ),
+//   new Question(
+//     "4?",
+//     ["Correct Answer", "answers", "answer", "answer"],
+//     "Correct Answer"
+//   ),
+//   new Question("5?", ["b", "Correct Answer", "ee", "ss"], "Correct Answer"),
+// ];
 
+
+// const startButton = document.getElementById("startButton");
+//   startButton.addEventListener("click", function(e) {
+//     console.log('clickedbutton')
+
+//   });
