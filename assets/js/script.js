fd6c4ab24a1acec690dff7d3412c176e05ddf1bc
Sara Wendland <info@rosebudsara.com>
2023-03-22 13:38:32 -0500

    added high score local storage and populated to high score tracker. resolved start button bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,21 +12,12 @@ var score = 0;
 var resetTimeUpButton = document.getElementById('resetTimeUp')
 var resetTimeUpContainer = document.getElementById('time-up-container');
 
-
-resetTimeUpButton.addEventListener('click', function(e) {
-  location.reload();
-}, false);
-
-
 var highScoreContainer = document.getElementById("high-score-container");
-
-
-startButton.addEventListener("click", startQuiz)
 
 var questions = [
   {
     question:"What is HTML short for?", 
-    choices: ["Hi There My Love", "He Took My Lettuce", "Hyper Theory Mopping Lines", "HyperText Markup Language"], 
+    choices: ["Hi There Mr. Lovecraft", "He Took My Lettuce", "Hyper Theory Mapping Lines", "HyperText Markup Language"], 
     answer: "HyperText Markup Language"
   }, 
   {
@@ -52,7 +43,7 @@ var questions = [
 ] 
 
 var quizTimer;
-
+// show questions when start button is clicked and iterate through them
 function renderQuestions() {
   var quizBox = document.createElement('div');
   var prompt = document.createElement('p');
@@ -69,7 +60,7 @@ function renderQuestions() {
   questionContainer.append(quizBox)
 
 }
- 
+//  what happens if user selects correct or incorrect answer/decrement time from incorrect
 function handleQuestionClick(e) {
 
   var questionClicked = e.target.textContent
@@ -84,19 +75,16 @@ function handleQuestionClick(e) {
   } else {
     console.log('incorrect')
       timeLeft -= 15;
-      alert('incorrect!'); 
-      
-                      
-
-  }
+      alert('incorrect!');       
+                  
+    }
 
   questionIndex++
 
   questionContainer.innerHTML = ""
 
   if (questionIndex > questions.length - 1) {
-    endQuiz();
-    
+    endQuiz();   
     
     
 
@@ -105,12 +93,9 @@ function handleQuestionClick(e) {
     renderQuestions()   
 
   }
-
   
-
-
   }
-
+// end quiz, show final score, show the high score entry form
 function endQuiz() {
   
   questionContainer.style.display = "none";
@@ -123,28 +108,30 @@ function endQuiz() {
 }
 
 
-startButton.addEventListener("click", startQuiz)
 
-questionContainer.addEventListener('click', handleQuestionClick)
-
+// push start button and start quiz renders questions, then hides the start button
 function startQuiz() {
   
   startTimer();
   renderQuestions();
+  startButton.style.display = "none";
   
    
 }
-
+// stop quiz when time runs out, show final score, hide question container
 function timeOut() {
     
   document.getElementById("timer").innerHTML = "Time Ran Out!";
       questionContainer.style.display = "none";
     scoreProgress.innerHTML = "Your Final Score: 0"
     resetTimeUpContainer.classList.remove('hide');
-    resetTimeUpButton.setAttribute("style", "width: 500px; height: 100px; background-color: lightgray; font-size: 30px;");
-
+    resetTimeUpButton.setAttribute("style", "width: 200px; height: 50px; border-radius: 10px; background-color: lightgray; font-size: 20px;");
 
 }
+
+
+
+// quiz timer 
 var quizTimer;
 var timeLeft = 45;
 function startTimer() {
@@ -153,12 +140,7 @@ function startTimer() {
   if (timeLeft <= 0) {
     clearInterval(quizTimer);
     timeOut();
-    
-    
-    // document.getElementById("timer").innerHTML = "Game Over!";
-    // document.createElement("reset").innerHTML = "reset quiz";
-    // questionContainer.style.display = "none";
-    // scoreProgress.innerHTML = "Your Final Score: 0" 
+   
                             
   } else {
     document.getElementById("timer").innerHTML =
@@ -170,30 +152,86 @@ function startTimer() {
   
 }, 1000);
 }
+// save scores and putsh them to local storage
+function saveScore() {
+  var savedInitials = document.getElementById("initials").value.trim();
+
+var highScores = JSON.parse(localStorage.getItem('highScores')) || []
+var newHighScore = {
+  initials: savedInitials, 
+  score: score
+}
+highScores.push(newHighScore)
+
+localStorage.setItem("highScores", JSON.stringify(highScores))
+
+renderHighScores(highScores);
+
+// tried this first to test local storage
+//   window.localStorage.setItem('score', score);
+// console.log('scorelog')
+
+// var savedInitials = document.getElementById("initials").value.trim();
+// console.log('saved initials')
+// window.localStorage.setItem('initials', savedInitials);
+}
+
+// populates high scores into an ordered list in the html sidebar on the top left
+function renderHighScores(scores) {
+  var scoreList = document.getElementById('high-score-list');
+  for (i = 0; i <= scores.length - 1; i++) {
+    var scoreItem = document.createElement("li")
+    scoreItem.textContent = `User: ${scores[i].initials} Score: ${scores[i].score}`;
+    scoreList.append(scoreItem);
+  }
+
+}
+// event listeners 
+
+startButton.addEventListener("click", startQuiz)
+
+questionContainer.addEventListener('click', handleQuestionClick)
+
+document.querySelector('#highScoreTracker').addEventListener("submit",function(e){
+  e.preventDefault();
+  saveScore();
+  var restartButton = document.createElement("button")
+  restartButton.textContent = "restart quiz"
+  highScoreContainer.append(restartButton)
+  restartButton.addEventListener('click', function(){ 
+    highScoreContainer.style.display = "none";
+    questionIndex = 0;
+    questionContainer.style.display = "block";
+    startQuiz()
+  })
+} )
+
+
+resetTimeUpButton.addEventListener('click', function(e) {
+  location.reload();
+}, false);
 
 
 
 
 
-// todo
-// create high score and user input stored data
+
+
+
+
+
+
 // const initials = prompt("Enter Your Initials");
 // document.getElementById("highScoreTracker").innerHTML = "High Score, " + initials;
 
-// decrement timer if answer is wrong-done
-// end timer when quiz has ended-done
 
 
 
 
 
 
-// start button function needs to start timer and populate questions-done
-// create questions-done
 
-// create reset function-done
-// loop questions-done
-// create questions-done
+// first try at questions
 // questions = [
   //   new Question(
     //     "this is question one?",
